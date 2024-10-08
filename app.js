3fc80f5ba3fb16894aa8b580a1cbf6435a739381
amanak84 <akkr4006@gmail.com>
2024-10-08 20:46:32 +0530

    Will update this soon

---
+++ app.js
@@ -0,0 +1,19 @@
+// let nav = document.querySelector("nav");
+// let channel = document.querySelector(".channel");
+// let support = document.querySelector(".supportContent")
+
+// nav.addEventListener("mouseover", ()=>{
+//     nav.style.backgroundColor = "#fff"
+// })
+
+let subscribe = document.querySelector(".sub");
+let channel = document.querySelector(".channel");
+
+
+subscribe.addEventListener("click", ()=>{
+    window.open("https://youtube.com/@priyakantju5611?si=sHPCsStiICC0D0e2");
+})
+
+channel.addEventListener("click", ()=>{
+    window.open("https://youtube.com/@priyakantju5611?si=sHPCsStiICC0D0e2");
+})
